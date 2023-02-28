a197d6784315371f7ca05c047dd41324037e97c3
Rachel Katz <rachel.katz@flatironschool.com>
2023-02-28 15:23:25 -0500

    Freebie, Company, and seeds done

--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer, ForeignKey)
+from sqlalchemy.orm import *
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 Base = declarative_base()
+
+#Questions for Company.give_freebie() and Company.oldest_company().
+    #should we be passing in session?
+
 
 class Company(Base):
     __tablename__ = 'companies'
@@ -12,14 +18,69 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    #create relationshp to freebie's foreign key column
+    freebies = relationship('Freebie', backref='company')
+    #create relationship to devs through freebies
+    devs = association_proxy('freebies', 'dev', 
+                        creator=lambda dv: Freebie(dev=dv))
+
+    ###
+    def give_freebie(self, dev, item_name, value, session):
+        freebie = Freebie(dev_id=dev.id, company_id=self.id, item_name=item_name, value=value)
+        session.add(freebie)
+        session.commit()
+        return freebie #TODO: are we supposed to be saving this to the database?, is it ok freebie.id == None?
+
+    @classmethod
+    def oldest_company(self, session):
+        return session.query(Company).order_by(Company.founding_year.asc()).first()
+    
     def __repr__(self):
-        return f'<Company {self.name}>'
+        return f'<Company id={self.id} name={self.name} founding_year={self.founding_year}>'
 
 class Dev(Base):
     __tablename__ = 'devs'
-
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    #create relationship to freebie's foreign key column
+     #NOTE USAGE OF backref and not back populates
+     #if we were to use back populates we would have to include it in Freebie()
+    freebies = relationship('Freebie', backref='dev')
+
+    #create relationship to companies through freebies
+    companies = association_proxy('freebies', 'company', 
+                                  creator=lambda cmpy: Freebie(company=cmpy))
+
+
+    ###
+    def received_one(self, item_name):
+        for freebie in self.freebies:
+            if freebie.item_name == item_name:
+                return True 
+        return False
+    
+    def give_away(self, dev, freebie):
+        pass 
+
     def __repr__(self):
-        return f'<Dev {self.name}>'
+        return f'<Dev id={self.id} name={self.name}>'
+
+
+#association table (the through table)
+class Freebie(Base):
+    __tablename__ = 'freebies'
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    #create foreign key columns 
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    ###
+    def print_details(self):
+        return f'{self.dev.name} owns a {self.item_name} from {self.company.name}'
+
+    def __repr__(self):
+        return f'<Freebie id={self.id} item_name={self.item_name}, value={self.value} dev_id={self.dev_id}, company_id={self.company_id}>'
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,62 @@
 #!/usr/bin/env python3
+from faker import Faker 
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker 
+from models import Dev, Company, Freebie 
+import random 
 
-# Script goes here!
+fake = Faker()
+
+#parameter x: takes in integer for number of companies to create
+#returns array of company instances created as well as their ids
+def create_companies(x):
+    companies = [] 
+    for i in range(x):
+        company = Company(name=fake.company(), founding_year=fake.year())
+        session.add(company) #add individually to get back id
+        session.commit()
+        companies.append(company)
+    return companies
+
+#parameter x: takes in integer for number of devs to create
+#returns array of dev instances created as well as their ids
+def create_devs(x):
+    devs = [] 
+    for i in range(x):
+        dev = Dev(name=fake.name())
+        session.add(dev) #add individually to get back id
+        session.commit() 
+        devs.append(dev)
+    return devs
+
+#x: number of freebies to make, 
+#companies: list of created companies WITH id,
+#devs: list of created companies WITH id
+def create_freebies(x, companies, devs):
+    freebies = [] 
+    for i in range(x):
+        freebie = Freebie(item_name=fake.word(), value=random.randint(30, 100), company_id=random.choice(companies).id, dev_id=random.choice(devs).id)
+        freebies.append(freebie)
+    session.bulk_save_objects(freebies) #can use bulk save because we don't need IDs (yet)
+    session.commit()
+    return freebies #will not include IDs
+
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///freebies.db')
+    Session = sessionmaker(bind=engine)
+    session = Session() 
+
+    session.query(Freebie).delete()
+    session.query(Dev).delete()
+    session.query(Company).delete() 
+
+    companies = create_companies(5)
+    devs = create_devs(10)
+    freebies = create_freebies(20, companies, devs)
+
+    #note: all session commits happen in create_model functions
+
+
+
+
+
